092e156080a5f8dc481a1c0fcf6195ce90a65339
Phil Smy <smyp@phil-smys-mac-pro.local>
2009-03-21 13:18:01 +0100

    add ability to specify hosts

--- lib/backgroundrb/bdrb_config.rb
+++ lib/backgroundrb/bdrb_config.rb
@@ -16,9 +16,14 @@ module BackgrounDRb
         opts.separator ""
         opts.on("-v","--version",
                 "Show version.") { $stderr.puts "1.1"; exit }
+        opts.separator ""
+        opts.on("-H", "--hostname=name", String,
+                "Specifies the hostname to operate under.",
+                "Default: #{Socket.gethostname.to_sym}") { |v| options[:hostname] = v }
       end.parse!(argv)
 
       ENV["RAILS_ENV"] = options[:environment] if options[:environment]
+      ENV["BDRB_HOSTNAME"] = options[:hostname] if options[:hostname]
     end
 
     def self.read_config(config_file)
@@ -31,6 +36,31 @@ module BackgrounDRb
         end
       else
         Object.const_set("RAILS_ENV",environment)
+      end
+
+      # block for deep_merging the hashes
+      deep_proc = Proc.new do |key, oldval, newval|
+        if oldval.kind_of?(Hash) && newval.kind_of?(Hash)
+          next oldval.merge(newval,&deep_proc)
+        end
+        next newval
+      end
+
+      if config[environment]
+        config.merge!( config[environment], &deep_proc)
+        if config[environment][:schedules]
+          config[:schedules].merge!( config[environment][:schedules], &deep_proc)
+        end
+      end
+
+      hostname = ENV["BDRB_HOSTNAME"].to_sym || Socket.gethostname.to_sym
+      if config[hostname]
+        config.merge!( config[hostname], &deep_proc)
+        if config[hostname][:schedules]
+          config[:schedules].merge!( config[hostname][:schedules], &deep_proc)
+        end
+      else
+        puts "Failed to find backgroundrb hostname #{hostname}"
       end
 
       ENV["RAILS_ENV"] = environment
